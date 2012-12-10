4dc9f58061f7cdb8192129126a883690229c488f
om <om@ubuntu.(none)>
2012-12-11 02:19:15 +0530

    tweeks

--- GetSongs.py
+++ GetSongs.py
@@ -1,10 +1,10 @@
 import os,re
 
 ###Get Latest movies/tracks###
-os.system("clear")
+os.system('clear')
 link='http://www.songspk.pk/'
 print '\nGetting latest Movies Please Wait...'
-os.system('wget '+link+' -O temp -o /dev/null')
+os.system('wget '+link+' -O ./temp -o /dev/null')
 tempFile = open('temp','r')
 tempMovies = tempFile.read()
 tempFile.close()
@@ -23,16 +23,17 @@ choice = int(raw_input('\nEnter Your Choice: '))-1
 
 ###Get Movie/Track Page###
 print '\nPlease wait getting file...'
-os.system('wget '+urls[choice]+' -O temp -o /dev/null')
+os.system('wget '+urls[choice]+' -O ./temp -o /dev/null')
 tempFile = open('temp','r')
 temp=tempFile.read()
 tempFile.close()
-links=re.findall(r'<a href=[^ ]*?songid=[0-9]*">[^0-9]*</a>',temp)
+links=re.findall(r'<a href=[^ ]*?songid=[0-9]*">[^<>]*</a>',temp)
+links+=re.findall(r'<a href=[^ ]*.mp3">[^<>]*</a>',temp)
 urls=[]
 names=[]
 for link in links:
 	urls.append(link.split('href="')[1].split('">')[0])
-	names.append(link.split('\t')[-1].split('</a>')[0])
+	names.append(link.split('">')[1].split('</a>')[0].replace('\n','').replace('\t',''))
 
 for i in range(len(names)):
 	print ' %2d. %s'%(i+1,names[i])
@@ -45,7 +46,6 @@ if choice=='*':
 		os.system('wget '+urls[i]+' -O '+'"'+names[i]+'.mp3"')
 else:
 	choice = int(choice)-1
-	print 'wget '+urls[choice]+' -O '+names[choice]
 	os.system('wget '+urls[choice]+' -O '+'"'+names[choice]+'.mp3"')
 
 os.system('rm temp')
